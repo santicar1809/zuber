dd663f0c55c41f65abcc75b629aac77e1d7ea3fd
santicar1809 <santicar1809@gmail.com>
2024-02-02 17:57:40 -0500

    update

--- analisis_sql.sql
+++ analisis_sql.sql
@@ -2,11 +2,11 @@
 Como los datos del proyecto se encuentran en la plataforma de TripleTen, todos los ejercicios de SQL serán ejecutados en la web de TripleTen.
 Febrero - 2024 */
 ----------------------------------------------------------------------------
--- Analisis de pregundas--
+-- Analisis de preguntas--
 /*1.  
 Encuentra la cantidad de viajes en taxi para cada compañía de taxis para el 15 y 16 de noviembre de 2017, asigna al campo resultante el nombre trips_amount e imprímelo también. 
 Ordena los resultados por el campo trips_amount en orden descendente.*/
-/*--------------Solucion-------------*/
+/*--------------Solución-------------*/
 SELECT 
     cabs.company_name,
     COUNT(cabs.company_name) AS trips_amount
@@ -26,7 +26,7 @@ que aparece cada nombre de cada compañia, entre el 15 de noviembre y el 16 de n
 /*2. 
 Encuentra la cantidad de viajes para cada empresa de taxis cuyo nombre contenga las palabras "Yellow" o "Blue" 
 del 1 al 7 de noviembre de 2017. Nombra la variable resultante trips_amount. Agrupa los resultados por el campo company_name.*/
-/*--------------Solucion-------------*/
+/*--------------Solución-------------*/
 SELECT 
     cabs.company_name, 
     COUNT(cabs.company_name) AS trips_amount
@@ -49,7 +49,7 @@ Del 1 al 7 de noviembre de 2017, las empresas de taxis más populares fueron Fla
 Encuentra el número de viajes de estas dos empresas y asigna a la variable resultante el nombre trips_amount. 
 Junta los viajes de todas las demás empresas en el grupo "Other". Agrupa los datos por nombres de empresas de taxis. 
 Asigna el nombre company al campo con nombres de empresas de taxis. Ordena el resultado en orden descendente por trips_amount.*/
-/*--------------Solucion-------------*/
+/*--------------Solución-------------*/
 SELECT 
     CASE WHEN cabs.company_name ='Flash Cab' THEN 'Flash Cab'
     WHEN cabs.company_name = 'Taxi Affiliation Services' THEN 'Taxi Affiliation Services'
@@ -70,7 +70,7 @@ y clasificar las demás como 'Otros', luego filtramos los valores desde el prime
 agrupando todo por el nuevo caso que hicimos y ordenandolo de mayor número de viajes a menor.*/
 /*4.
 Recupera los identificadores de los barrios de O'Hare y Loop de la tabla neighborhoods.*/ 
-/*--------------Solucion-------------*/
+/*--------------Solución-------------*/
 SELECT 
     neighborhood_id,name
 FROM
@@ -85,7 +85,7 @@ Usando el operador CASE, divide todas las horas en dos grupos:
 Bad si el campo description contiene las palabras rain o storm, y Good para los demás. 
 Nombra el campo resultante weather_conditions. 
 La tabla final debe incluir dos campos: fecha y hora (ts) y weather_conditions.*/
-/*--------------Solucion-------------*/
+/*--------------Solución-------------*/
 SELECT
     ts,
     CASE WHEN description LIKE '%rain%' THEN 'Bad'
@@ -111,7 +111,7 @@ Las columnas de la tabla deben estar en el siguiente orden:
  - weather_conditions
  - duration_seconds
  - Ordena por trip_id.*/
-/*--------------Solucion-------------*/
+/*--------------Solución-------------*/
 SELECT
     trips.start_ts,
     T.weather_conditions,
